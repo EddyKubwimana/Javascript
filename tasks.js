5f83312dd0431d06bb117fec0fab50749ef0995a
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-05-08 22:36:12 +0000

    changes

--- tasks.js
+++ tasks.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
+   document.querySelector("#button").disabled= true;
+   document.querySelector("#item").onkeyup = function(){
+    document.querySelector("#button").disabled = false;
+   }
    document.querySelector("form").onsubmit= function(){
 
+
     const task = document.querySelector("#item").value;
-    const timeline = document.querySelector("#timeline").value;
     const newtask =  document.createElement("li");
-    const timetask = document.createElement("li")
     newtask.innerHTML = task;
-    timetask. innerHTML = timetask;
     document.querySelector("#task").append(newtask);
+    document.querySelector("#item").value = '';
+    document.querySelector("#button").disabled = true;
+
+    return false;
 
 
    }
