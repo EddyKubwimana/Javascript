11aad674a6bdd007c936daa8504b7376adafc91f
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-05-08 19:26:48 +0000

    changed the css

--- button.js
+++ button.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     document.querySelectorAll("button").forEach(function(button){
-        document.querySelector("h1").style.color = button.dataset.color;
+        button.onclick = function(){document.querySelector("h1").style.color = button.dataset.color;};
+        
     });
 });
