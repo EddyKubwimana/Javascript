0ee14394b723df192f8e4543151d66b849a07fa8
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-05-09 23:54:04 +0000

    new file

--- counter.js
+++ counter.js
@@ -1,24 +1,37 @@
-let counter = 0;
-        function up(){
+if (!localStorage.getItem("count")){
+  localStorage.setItem("count", 0);
+}
+
+function up(){
+            let count = localStorage.getItem("count");
+            count++;
             let heading = document.querySelector("h1");
-            heading.innerHTML = counter;
-            counter+=1;
+            heading.innerHTML = count;
+           
             let button = document.querySelector("button")
             button.innerText = "Count up"
+            localStorage.setItem("count", count)
         
             if (heading.innerHTML == 20){
               heading.innerHTML = "The counting is over";
               button.innerText ="Refresh";
-              counter = 0
+              localStorage.setItem("count", 0)
 
-            };
+        };
+
+          
     
         
     };
 
+
     document.addEventListener("DOMContentLoaded",function(){
 
+      document.querySelector("h1").innerHTML = localStorage.getItem("count");
+
         
-    document.querySelector("button").onclick =up;
+    document.querySelector("button").onclick = up;
+
+    //setInterval(up, 1000);
 
     });
