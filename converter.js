30358935823b269fc9421b2e69637d6da6223b67
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-05-10 01:25:15 +0000

    changes

--- converter.js
+++ converter.js
@@ -1,17 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(){
-    var myHeaders = new Headers();
+
+    // api call to fetch data on currency exchange
+    var myHeaders = new Headers(); 
     myHeaders.append("apikey", localStorage.getItem("api"));
-    
     
     var requestOptions = {
       method: 'GET',
       redirect: 'follow',
       headers: myHeaders
     };
+    var keys = new Array();
     
     fetch("https://api.apilayer.com/exchangerates_data/latest", requestOptions)
       .then(response => response.json())
-      .then(result => console.log(result))
+      .then(result =>{
+        console.log(result)
+    
+        keys = Object.keys(result.rates)
+        console.log(keys)
+
+      })
       .catch(error => console.log('error', error));
 
 
